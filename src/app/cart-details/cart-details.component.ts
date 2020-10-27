5814f41d7d9bbb4ee631aade3d160feb1320125a
Matthias Burger <github@family-burger.de>
2020-10-27 14:27:19 +0100

    component to update data

--- src/app/cart-details/cart-details.component.ts
+++ src/app/cart-details/cart-details.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from '../store/cart.model';
 
 @Component({
   templateUrl: './cart-details.component.html',
 })
 export class CartDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public cart: Cart) { }
 
   ngOnInit(): void {
   }
